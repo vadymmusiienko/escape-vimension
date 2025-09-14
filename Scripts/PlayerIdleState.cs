49ec33143140b65b4e14d0e4369f7274629a6f08
Anonymous <anon@example.com>
2025-09-14 18:26:57 +1000

    StateMachine is done
    Basic movement of player is implemented using state machine. Also I've created some example material to test.

---
+++ Scripts/PlayerIdleState.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerIdleState : PlayerState
+{
+    public PlayerIdleState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        player.ZeroVelocity();
+    }
+
+    public override void Exit() { base.Exit(); }
+
+    public override void Update()
+    {
+        base.Update();
+        if (xInput != 0 || zInput != 0)
+        {
+            stateMachine.ChangeState(player.moveState);
+        }
+    }
+}
