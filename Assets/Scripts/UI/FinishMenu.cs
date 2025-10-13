6ee5be7adccb3c1ad854187c50f752a350ee2ec6
Vadym Musiienko <vmusiienko@student.unimelb.edu.au>
2025-10-13 16:15:28 +1100

    Finish and death scenes

---
+++ Assets/Scripts/UI/FinishMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishMenu : MonoBehaviour 
+{
+    public void RestartGame()
+    {
+        // Load the main game scene
+        SceneManager.LoadScene("MainScene");
+    }
+    
+    public void QuitGame()
+    {
+        // Quit the application
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+}
